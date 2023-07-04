2fb92932e395d89f7ba084a3d1f65f83be7a096f
Shehbaz Mirza <shehbaz.mirza999@gmail.com>
2023-07-05 00:43:17 +0530

    updated fetch method

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -9,7 +9,7 @@ export const metadata = {
 }
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
+  const res = await fetch("http://127.0.0.1:3000/api/posts", {
     cache: "no-store",
   });
 
